d11a1966fd40c24177543072a1ee30e891e7796f
Artem <koptelovu@gmail.com>
2023-11-27 11:21:19 +0300

    Реализована возможность жаловаться на вопросы через команду /report в классе Commands

--- src/main/java/tg/project/TelegramGameBot/service/Commands.java
+++ src/main/java/tg/project/TelegramGameBot/service/Commands.java
@@ -1,4 +1,5 @@
 package tg.project.TelegramGameBot.service;
+import java.io.*;
 
 /**
  * Класс, реализующий функцию анализа команд, отправленной пользователем
@@ -14,13 +15,31 @@ public class Commands
             case "/help":
                 res[0] = """
                             Бот предствляет собой тривиальную игру, где нужно сосчитать результат предложенного примера
-                            /result - посмотреть количество Ваших правильных ответовю.
+                            /result - посмотреть количество Ваших правильных ответов
+                            /report текст вопроса - пожаловаться на вопрос
                             """;
                 res[1] = null;
                 break;
             case "/result":
                 res[0] = "количество правильных ответов: " + user.getCorrectAnswers() + " из " + user.getNumberOfQuestions();
                 res[1] = null;
+                break;
+            case "/report":
+                res[0] = "приносим свои извинения, обязательно проверим вопрос";
+                res[1] = null;
+                StringBuilder text = new StringBuilder();
+                for (int i = 1; i < command.length; ++i)
+                    text.append(command[i]).append(' ');
+                try(FileWriter writer = new FileWriter("Жалобы.txt", false))
+                {
+                    writer.write(String.valueOf(text) + '\n');
+
+                    writer.flush();
+                }
+                catch(IOException ex){
+
+                    System.out.println(ex.getMessage());
+                }
                 break;
             default:
                 res[0] = "Неверная команда";
